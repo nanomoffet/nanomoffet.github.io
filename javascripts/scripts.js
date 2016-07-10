25d6efe6a5b0c7535e178555977c8f760f82365d
Michael Moffet <michael.moffet@plasticsurgerystudios.com>
2016-07-10 16:56:04 -0700

    add alert to confirm message sent

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -14,3 +14,14 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    $("[type=submit]").click(function() {
+        var x = document.forms["contact-form"]["name"].value;
+        var y = document.forms["contact-form"]["_replyto"].value;
+
+        if (x !== null && x !=="" && y !== null && y !=="") {
+            alert("Thank you for contacting me, I'll get back to you as soon as I can!");
+        }
+    });
+});
